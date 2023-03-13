63cf69eceeb81289c8625fbb4cf175b8fe1ba2e8
JoseJoenk <jmjoenk@gmail.com>
2023-03-13 15:03:16 -0300

    Finalizando o Ex1 da aula 15
    Socorro, eu demorei muito

--- Estudos/javascript/aula15ex/ex001.js
+++ Estudos/javascript/aula15ex/ex001.js
@@ -1,11 +1,28 @@
 function contar() {
-    var ini = document.getElementById('ini')
-    var fim = document.getElementById('fim')
-    var feet = document.getElementById('feet')
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
-    for (num = ini; num <= fim; num =+ feet) {
-        res.innerHTML = res.innerHTML + `${num.value} => `
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Não vai dar não...'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449} `
+        }
+        }else {
+            for(var c = i; c >= f; c -= p)
+                res.innerHTML += `${c} \u{1F449} `
+        }
+        res.innerHTML += ` \u{1F3C1}`
     }
-
-   /* res.innerText = `Começa no ${ini.value}, termina no ${fim.value} e anda a cada ${feet.value}!`*/
 }
